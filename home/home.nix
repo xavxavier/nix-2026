4f670c8274c3868732fc2bf668a620d7d51a836b
Xavier <xavier.brasher@gmail.com>
2026-01-01 17:10:36 +1100

    1

--- home/home.nix
+++ home/home.nix
@@ -26,10 +26,9 @@
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     hello
-
     neofetch
-
     bat
+    cvt
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
---
+++ programs/sway/default.nix
@@ -0,0 +1,18 @@
+{config, pkgs, ...}:
+
+{
+	wayland.windowManager.sway = {
+		enable = true; wrapperFeatures.gtk = true; # Fixes common issues with GTK 3 apps
+		config = rec {
+			modifier = "Mod4";
+			# Use kitty as default terminal
+			terminal = "kitty"; 
+			startup = [
+				# Launch Firefox on start
+				{command = "firefox";}
+			];
+		};
+	};
+
+ 
+}
