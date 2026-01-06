fa4f777d3722d8a8306b01e19a6a134b358ac4ae
Xavier B <xavier.brasher@gmail.com>
2026-01-06 23:51:43 +1100

    Moved my config to real hardware

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A very basic flake";
+  description = "My NIXOS";
 
   inputs = {
   	nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
@@ -35,22 +35,6 @@
 
 			};
 		};
-    packages.gillsans = pkgs.stdenvNoCC.mkDerivation {
-          name = "gillsans-font";
-          dontConfigue = true;
-          src = pkgs.fetchzip {
-            url =
-              "https://freefontsvault.s3.amazonaws.com/2020/02/Gill-Sans-Font-Family.zip";
-            sha256 = "sha256-YcZUKzRskiqmEqVcbK/XL6ypsNMbY49qJYFG3yZVF78=";
-            stripRoot = false;
-          };
-          installPhase = ''
-            mkdir -p $out/share/fonts
-            cp -R $src $out/share/fonts/opentype/
-          '';
-          meta = { description = "A Gill Sans Font Family derivation."; };
-        };
-
 	};
 
   }
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -2,18 +2,13 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}:
+{ config, lib, pkgs, ... }:
 
 {
-  imports = [
-    # Include the results of the hardware scan.
-    ./hardware-configuration.nix
-  ];
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
@@ -21,202 +16,75 @@
 
   networking.hostName = "nixos"; # Define your hostname.
 
-  virtualisation.vmware.guest.enable = true;
-
   # Configure network connections interactively with nmcli or nmtui.
   networking.networkmanager.enable = true;
 
   # Set your time zone.
   time.timeZone = "Australia/Melbourne";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-  console = {
-    font = "Lat2-Terminus16";
-    keyMap = "us";
-    #   useXkbConfig = true; # use xkb.options in tty.
-  };
-
-  fonts.packages = with pkgs; [
-    nerd-fonts.fira-code
-    nerd-fonts.droid-sans-mono
-  ];
-
-  # Enable the X11 windowing system.
   services.xserver = {
     enable = true;
-    resolutions = [
-      {
-        x = 2560;
-        y = 1440;
-      }
-    ];
-    windowManager.i3 = {
-      enable = true;
-      extraPackages = with pkgs; [
-        dmenu
-        i3status
-        i3blocks
-      ];
-    };
+    desktopManager.xterm.enable = false;
+    autoRepeatDelay = 200;
+    autoRepeatInterval = 35;
+    windowManager.i3.enable = true;
   };
 
-  services.picom = {
-    enable = true;
-    fade = true;
-    #    vSync = true;
-    shadow = true;
-    fadeDelta = 4;
-    inactiveOpacity = 0.8;
-    activeOpacity = 1;
-    #    backend = "glx";
-    settings = {
-      blur = {
-        #method = "dual_kawase";
-        #	background = true;
-        strength = 5;
-      };
-    };
+  services.displayManager ={
+    defaultSession = "none+i3";
   };
-
-  services.displayManager.defaultSession = "none+i3";
-
-  programs.sway.enable = true;
-
-  xdg.portal = {
-    enable = true;
-    wlr.enable = true; # Enables xdg-desktop-portal-wlr for screen sharing, etc.
-    extraPortals = [ pkgs.xdg-desktop-portal-gtk ]; # For GTK apps to use the portal
-  };
-
-  security.polkit.enable = true;
-
-  virtualisation.docker = {
-    enable = true;
-  };
-
-  # Configure keymap in X11
-  services.xserver.xkb.layout = "us";
-  services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  services.pipewire.enable = false;
-  # Enable sound.
-  services.pulseaudio.enable = true;
-  # OR
-  #services.pipewire = {
-  # enable = true;
-  # pulse.enable = true;
-  #};
-
-  programs.zsh.enable = true;
-
-  users.users.xavier = {
-    isNormalUser = true;
-    extraGroups = [
-      "wheel"
-      "docker"
-    ]; # Enable ‘sudo’ for the user.
-    shell = pkgs.zsh;
-    packages = with pkgs; [
-      tree
-    ];
+  # services.pulseaudio.enable = true;
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
   };
 
-  programs.firefox.enable = true;
+  services.libinput.enable = true;
 
-  # List packages installed in system profile.
-  # You can use https://search.nixos.org/ to find more packages (and options).
+  users.users.xavier = {
+	  isNormalUser = true;
+	  extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+	  shell = pkgs.zsh;
+	  packages = with pkgs; [
+		  tree
+	  ];
+  };
+
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    neovim
+    vim 
     wget
-    alacritty
-    firefox
-    htop
-    zsh
+    neovim
     git
-    xorg.xrandr
-    xorg.libxcvt
-    foot
-    gcc
-    clang
-    unzip
-    python312
-    python3
-    cifs-utils
-    psmisc
+    killall
+    kitty
+    firefox
+    dmenu
+    i3status
+    alacritty
+    zsh
     hyprland
-    picom
+    
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
+  programs.zsh.enable = true;
   programs.mtr.enable = true;
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
   };
 
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-  };
-  programs.hyprland = {
-    enable = true;
-    xwayland.enable = true;
-  };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
   networking.firewall.enable = false;
 
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.fira-code
+  ];
 
-  # nix.settings.experimental-features = ["nix-command" "flake"];
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "25.11"; # Did you read the comment?
-
-  nix = {
-    # package = pkgs.nixFlakes;
-    # extraOptions = "experimental-feaures = nix-command flakes";
-    settings.experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
-  };
-
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+  system.stateVersion = "25.11"; 
 }
+
