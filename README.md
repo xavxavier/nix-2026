3b7f1d6f582ec3020604e0e6dee3a6827e994c38
Xavier B <xavier.brasher@gmail.com>
2026-01-07 19:01:41 +1100

    made some slight changes

---
+++ README.md
@@ -0,0 +1,9 @@
+# Readme
+
+wallpaper goes into ~/.wallpapers/image.jpg this can be changed in ./programs/hyprland/hyprpaper.nix
+
+alliases: 
+- flakeupdate - pulls and updates nix pkgs, home-manager and nvim config 
+- nixupdate - sudo nixos-rebuild switch --flake ~/.dotfiles
+- homeupdate - home-manager switch --flake ~/.dotfiles
+
--- programs/zsh/default.nix
+++ programs/zsh/default.nix
@@ -1,43 +1,33 @@
-{config, pkgs, ...}:
-{
-  programs.zoxide = {
-    enable = true;
-  };
+{ config, pkgs, ... }:
+
+let dotfiles = "${config.home.homeDirectory}/.dotfiles";
+in {
+  programs.zoxide = { enable = true; };
 
   programs.zsh = {
-  	enable = true;
-	autosuggestion.enable = true;
-	syntaxHighlighting.enable = true;
-	localVariables = {
-		"EDITOR" = "nvim";
-	};
+    enable = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    localVariables = { "EDITOR" = "nvim"; };
 
-	shellAliases = {
-    "ls" = "eza";
-		"ll" = "eza -l";
-		"l" = "eza -la";
-		"la" = "eza -la";
-		".." = "cd ..";
-    "flakeupdate" = "nix flake update --flake /home/xavier/.dotfiles";
-		"nixupdate" = "sudo nixos-rebuild switch --flake /home/xavier/.dotfiles/.";
-		"homeupdate" = "home-manager switch --flake /home/xavier/.dotfiles/.";
-		"vim" = "nvim";
-    "cd" = "z";
-	};
+    shellAliases = {
+      "ls" = "eza";
+      "ll" = "eza -l";
+      "l" = "eza -la";
+      "la" = "eza -la";
+      ".." = "cd ..";
+      "flakeupdate" = "nix flake update --flake ${dotfiles}";
+      "nixupdate" = "sudo nixos-rebuild switch --flake ${dotfiles}";
+      "homeupdate" = "home-manager switch --flake ${dotfiles}";
+      "vim" = "nvim";
+      "cd" = "z";
+    };
 
-	oh-my-zsh = {
-		enable = true;
-		theme = "robbyrussell";
-		plugins = [
-			"sudo"
-			"rust"
-			"pip"
-			"tailscale"
-			"docker"
-			"ngrok"
-			"git"
-		];
-			
-	};
+    oh-my-zsh = {
+      enable = true;
+      theme = "robbyrussell";
+      plugins = [ "sudo" "rust" "pip" "tailscale" "docker" "ngrok" "git" ];
+
+    };
   };
 }
